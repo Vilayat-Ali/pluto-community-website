7956d51780c746e9e79e9907064baeb6ee430f65
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2022-02-25 16:49:55 +0530

    Django API integration in team page

--- src/pages/Inner/Team.tsx
+++ src/pages/Inner/Team.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 // Components 
 import SearchBar from "../../component/SearchBar";
 import TeamCard from '../../component/Cards/TeamCard';
+import axios from 'axios';
 
 export default function Team() {
 
@@ -12,40 +13,17 @@ export default function Team() {
     const [searchTerm, setSearchTerm] = useState('');
 
 
-    const demoTeam = [
-        {
-            name: 'Syed Vilayat Ali Rizvi',
-            role: 'Founder',
-            image: '',
-            twitter: '',
-            github: 'https://www.github.com/Vilayat-Ali',
-            linkedin: ''
-        },
-        {
-            name: 'Member 1',
-            role: 'Software Engineer',
-            image: '',
-            twitter: '',
-            github: 'https://www.github.com/Vilaya-Ali',
-            linkedin: ''
-        },
-        {
-            name: 'Member 2',
-            role: 'Founder Manager',
-            image: '',
-            twitter: '',
-            github: 'https://www.github.com/Vilaya-Ali',
-            linkedin: ''
-        },
-        {
-            name: 'Member 3',
-            role: 'Manager Founder',
-            image: '',
-            twitter: '',
-            github: 'https://www.github.com/Vilayat-Ali',
-            linkedin: ''
-        }
-    ];
+    async function fetchData(){
+        await axios.get("http://127.0.0.1:8000/api/member/").then(res => {
+            setTeam(res.data)
+        }).catch((err:any) => {
+            console.log(err.message)
+        })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
   return (
     <>
@@ -57,30 +35,30 @@ export default function Team() {
                 <option value="Role">Role</option>
         </SearchBar>
 
-        <div className="row">
+        <div className="row mx-auto">
 
-          { demoTeam.filter(function(memberData){
+          { team.filter(function(memberData: any){
               if(searchTerm === ''){
                   return memberData;
               }
               if(searchTerm !== ''){
-                  if(sortBy === '' && (memberData.name.toLowerCase().includes(searchTerm.toLowerCase()) || memberData.role.toLowerCase().includes(searchTerm.toLowerCase()))){
+                  if(sortBy === '' && (memberData.full_name.toLowerCase().includes(searchTerm.toLowerCase()) || memberData.role.toLowerCase().includes(searchTerm.toLowerCase()))){
                       return memberData;
                   }
-                  if(sortBy === 'Name' && memberData.name.toLowerCase().includes(searchTerm.toLowerCase())){
+                  if(sortBy === 'Name' && memberData.full_name.toLowerCase().includes(searchTerm.toLowerCase())){
                       return memberData;
                   }
                   if(sortBy === 'Role' && memberData.role.toLowerCase().includes(searchTerm.toLowerCase())){
                     return memberData;
                   }
               }
-          }).map(function(member){
+          }).map(function(member: any){
               return (
 
                     <TeamCard 
-                        key={member.name}
-                        name={member.name}
-                        image={"https://bootdey.com/img/Content/avatar/avatar1.png"}
+                        key={member.full_name}
+                        name={member.full_name}
+                        image={member.image}
                         role={member.role}
                         github={member.github}
                         twitter={member.twitter}
